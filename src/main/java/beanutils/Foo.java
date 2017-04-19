59681eeb1dda9351a5b1408f9a26c68d61aa011d
inCompany <inCompany@qq.com>
2017-04-19 08:51:28 +0800

    'v0.2'

---
+++ src/main/java/beanutils/Foo.java
@@ -0,0 +1,7 @@
+package beanutils;
+
+/**
+ * Created by cjp on 2017/4/18.
+ */
+public class Foo {
+}
---
+++ src/main/java/beanutils/FooTest.java
@@ -0,0 +1,10 @@
+package beanutils;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by cjp on 2017/4/18.
+ */
+public class FooTest {
+
+}
